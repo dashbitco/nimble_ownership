7cfdba78af9821c8542897c3b1e1bcb7e4430d00
Andrea Leopardi <an.leopardi@gmail.com>
2024-01-20 16:31:30 +0100

    Release v0.2.0

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Changelog
+
+## v0.2.0
+
+This release contains mostly breaking changes, and a first sort-of-stable API. It also adds *shared mode*.
--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule NimbleOwnership.MixProject do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.2.0"
   @repo "https://github.com/dashbitco/nimble_ownership"
 
   def project do
